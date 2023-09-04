e3f1f73652b4ba1f6a21a945d724e9a5d5d3bd58
6531503008 <122780034+kongphop1209@users.noreply.github.com>
2023-09-04 09:36:23 +0000

    cmmit

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -1,28 +1,55 @@
 package th.mfu;
 
 import java.io.IOException;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-//TODO: add webservlet to "/calbmi"
-public class BMICalculatorServlet extends HttpServlet{
+@WebServlet("/calbmi")
+public class BMICalculatorServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO: get parameter from request: "weight" and "height"
-        
-        //TODO: calculate bmi
+        String weightStr = request.getParameter("weight");
+        String heightStr = request.getParameter("height");
 
-        //TODO: determine the built from BMI
-      
-        //TODO: add bmi and built to the request's attribute
+        if (weightStr != null && !weightStr.isEmpty() && heightStr != null && !heightStr.isEmpty()) {
+            try {
+                double weight = Double.parseDouble(weightStr);
+                double height = Double.parseDouble(heightStr);
 
-        //TODO: forward to jsp
-           
+                double bmi = calculateBMI(weight, height);
+                String build = determineBuild(bmi);
+
+                request.setAttribute("bmi", Math.round(bmi));
+                request.setAttribute("build", build);
+
+                request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
+            } catch (NumberFormatException e) {
+                response.getWriter().println("Invalid weight or height values.");
+            }
+        } else {
+            response.getWriter().println("Please provide weight and height parameters.");
+        }
     }
-    
+
+    private double calculateBMI(double weight, double height) {
+        return weight / ((height)*(height));
+    }
+
+    private String determineBuild(double bmi) {
+        if (bmi < 18.5) {
+            return "underweight";
+        } else if (bmi < 25) {
+            return "normal weight";
+        } else if (bmi < 30) {
+            return "overweight";
+        } else if (bmi < 35) {
+            return "obese";
+        } else {
+            return "extremely obese";
+        }
+    }
 }
